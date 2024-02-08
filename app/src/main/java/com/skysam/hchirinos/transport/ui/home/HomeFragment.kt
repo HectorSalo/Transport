c1b2add4db9b41f0767b6596b1df6b6df5a93724
hchirinos <hchirinos@ubiipagos.com>
2024-02-08 16:38:48 -0400

    redesign

--- app/src/main/java/com/skysam/hchirinos/transport/ui/home/HomeFragment.kt
+++ app/src/main/java/com/skysam/hchirinos/transport/ui/home/HomeFragment.kt
@@ -11,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
@@ -134,7 +135,6 @@ class HomeFragment : Fragment() {
         sheetView = binding.sheetInclude.bottomSheet
         val bottomSheetBehavior: BottomSheetBehavior<*>?
         bottomSheetBehavior = BottomSheetBehavior.from(sheetView)
-
     }
 
     /*private fun showSeats() {
